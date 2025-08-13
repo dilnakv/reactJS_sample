c63a8dd3b99020511e62a1ddbab99f6f105d26d2
Dilna K Vasavan <dilna@xminds.com>
2025-08-13 11:34:40 +0530

    Login using useState

--- src/componet/Login.tsx
+++ src/componet/Login.tsx
@@ -1,16 +1,18 @@
 import React, { useRef, useState } from "react";
 
 export const Login = () => {
-  const usernameRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+  //   const usernameRef = useRef<HTMLInputElement>(null);
+  //   const passwordRef = useRef<HTMLInputElement>(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState<string>("");
   const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const username = usernameRef.current?.value || "";
-    const password = passwordRef.current?.value || "";
+    // const username = usernameRef.current?.value || "";
+    // const password = passwordRef.current?.value || "";
 
     if (username === "admin" && password === "xminds@123") {
       setMessage("Login Successful");
@@ -27,11 +29,23 @@ export const Login = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Username: </label>
-          <input type="text" ref={usernameRef} style={{ width: 200 }} />
+          {/* <input type="text" ref={usernameRef} style={{ width: 200 }} /> */}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={{ width: 200 }}
+          />
         </div>
         <div style={{ marginTop: "10px" }}>
           <label>Password : </label>
-          <input type="password" ref={passwordRef} style={{ width: 200 }} />
+          {/* <input type="password" ref={passwordRef} style={{ width: 200 }} /> */}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ width: 200 }}
+          />
         </div>
         <button
           type="submit"
